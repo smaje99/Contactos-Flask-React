592c589072adc083593d5542bb1624a4efd145c0
Sergio Maje <smajefranco@gmail.com>
2021-01-15 16:17:07 -0500

    Menu de navegacion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,18 @@ import {
   BrowserRouter as Router,
   Switch,
   Route
-} from 'react-router-dom'
+} from 'react-router-dom';
 
-import { About } from './components/About'
-import { Users } from './components/Users'
+import { About } from './components/About';
+import { Users } from './components/Users';
+import { NavBar } from './components/Navbar';
 
 import './App.css';
 
 function App() {
   return (
     <Router>
-
+      <NavBar />
       <div>
         <Switch>
           <Route path="/about" component={ About } />
