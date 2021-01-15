1708866fdf89c9700de1acb81d2b693ec0566a36
Sergio Maje <smajefranco@gmail.com>
2021-01-15 15:51:02 -0500

    Rutas y componentes
    * Componente About
    * Componente Navbar
    * Componente Users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import { About } from './components/About'
+import { Users } from './components/Users'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+
+      <div>
+        <Switch>
+          <Route path="/about" component={ About } />
+          <Route path="/" component= { Users } />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Navbar = () => (
+    <h1>Navbar</h1>
+)
