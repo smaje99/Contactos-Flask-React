440bb91b1a60185007aafc176194f9872bc31db5
Sergio Maje <smajefranco@gmail.com>
2021-01-15 18:12:16 -0500

    Booststrap SuperHero style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
+import 'normalize.css/normalize.css';
 import './index.css';
 import App from './App';
+
+import 'bootswatch/dist/superhero/bootstrap.min.css'
 
 ReactDOM.render(
   <App />,
